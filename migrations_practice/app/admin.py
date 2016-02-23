74deb19eca4bc76a636dbf6a819ae90975397bc4
joegotflow83 <joe@absolutod.com>
2016-02-23 14:53:39 -0500

    Added Materialize

--- migrations_practice/app/admin.py
+++ migrations_practice/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PlayerStat
+
+
+@admin.register(PlayerStat)
+class PlayerStatAdmin(admin.ModelAdmin):
+
+
+	pass
